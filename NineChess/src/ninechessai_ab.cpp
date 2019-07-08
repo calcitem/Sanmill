f700aa3d70e304ced2a2d99d74116cfdc9892fe7
CalciteM <calcitem@outlook.com>
2019-07-09 01:06:14 +0800

    Enable IDS

--- NineChess/src/ninechessai_ab.cpp
+++ NineChess/src/ninechessai_ab.cpp
@@ -508,8 +508,8 @@ int NineChessAi_ab::changeDepth(int originalDepth)
     if ((chessTemp.context.stage) & (NineChess::GAME_PLACING)) {
 #ifdef GAME_PLACING_DYNAMIC_DEPTH
 #ifdef DEAL_WITH_HORIZON_EFFECT
-        //int depthTable[] = { 2, 11, 11, 11, 11,  10,  9, 8, 8, 8, 7, 7, 1 };
-        int depthTable[] = { 2, 12, 12, 12, 12, 11, 10, 9, 9, 8, 8, 7, 1 };
+        int depthTable[] = { 2, 11, 11, 11, 11, 10,  9, 8, 8, 8, 7, 7, 1 };
+          //int depthTable[] = { 2, 12, 12, 12, 12, 11, 10, 9, 9, 8, 8, 7, 1 };
 #else
         //int depthTable[] = { 2, 12, 12, 12, 12, 11, 10, 9, 8, 8, 8, 7, 1 };
           int depthTable[] = { 2, 13, 13, 13, 12, 11, 10, 9, 9, 8, 8, 7, 1 };
