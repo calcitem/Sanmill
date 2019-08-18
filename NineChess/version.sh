c0132b630e985d8167e7bcef467203cb7e2897c1
CalciteM Team <calcitem@outlook.com>
2019-08-18 08:10:54 +0800

    Add version.sh

---
+++ NineChess/version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -f include/version.h
+
+git rev-list HEAD | sort > config.git-hash
+
+LOCALVER=`wc -l config.git-hash | awk '{print $1}'`
+
+if [ $LOCALVER \> 1 ] ; then
+    VER=`git rev-list origin/master | sort | join config.git-hash - | wc -l | awk '{print $1}'`
+    if [ $VER != $LOCALVER ] ; then
+        VER="$VER+$(($LOCALVER-$VER))"
+    fi
+    if git status | grep -q "modified:" ; then
+        VER="${VER}M"
+    fi
+    VER="$VER $(git rev-list HEAD -n 1 | cut -c 1-7)"
+    GIT_VERSION=r$VER
+else
+    GIT_VERSION=
+    VER="x"
+fi
+
+rm -f config.git-hash
+
+cat version.h.template | sed "s/\$FULL_VERSION/$GIT_VERSION/g" > include/version.h
+
+echo "Generated version.h"
