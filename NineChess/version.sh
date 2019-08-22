59aa5abfd2ff07aa538295c837e161aa5b2af61f
CalciteM Team <calcitem@outlook.com>
2019-08-23 00:34:51 +0800

    version: 将最近的 git tag 纳入版本号显示

--- NineChess/version.sh
+++ NineChess/version.sh
@@ -9,6 +9,7 @@ rm -f $VERSION_H
 git rev-list HEAD | sort > config.git-hash
 
 LOCALVER=`wc -l config.git-hash | awk '{print $1}'`
+TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 
 if [ $LOCALVER \> 1 ] ; then
     VER=`git rev-list origin/$GIT_BRANCH | sort | join config.git-hash - | wc -l | awk '{print $1}'`
@@ -19,7 +20,7 @@ if [ $LOCALVER \> 1 ] ; then
         VER="${VER}M"
     fi
     VER="$VER g$(git rev-list HEAD -n 1 | cut -c 1-7)"
-    GIT_VERSION=r$VER
+    GIT_VERSION="$TAG r$VER"
 else
     GIT_VERSION=
     VER="x"
