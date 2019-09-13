940f1190614c7e5774613c664e97c6bac04b95f7
CalciteM Team <calcitem@outlook.com>
2019-09-13 12:37:29 +0800

    refactor: Add square_t for feature

--- src/game/types.h
+++ src/game/types.h
@@ -40,6 +40,16 @@ enum move_t : int32_t
     //MOVE_NULL = 65
 };
 
+enum square_t : int
+{
+    SQ_0, SQ_1, SQ_2, SQ_3, SQ_4, SQ_5, SQ_6, SQ_7,
+    SQ_D5, SQ_E5, SQ_E4, SQ_E3, SQ_D3, SQ_C3, SQ_C4, SQ_C5,
+    SQ_D6, SQ_F6, SQ_F4, SQ_F2, SQ_D2, SQ_B2, SQ_B4, SQ_B6,
+    SQ_D7, SQ_G7, SQ_G4, SQ_G1, SQ_D1, SQ_A1, SQ_A4, SQ_A7,
+    SQ_32, SQ_33, SQ_34, SQ_35, SQ_36, SQ_37, SQ_38, SQ_39,
+    SQ_COUNT
+};
+
 // 移动方向，包括顺时针、逆时针、向内、向外4个方向
 enum direction_t
 {
