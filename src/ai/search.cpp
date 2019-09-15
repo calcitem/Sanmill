b330dd28ce5ed097ef57f1f1d91d496bb953a3cd
CalciteM Team <calcitem@outlook.com>
2019-09-15 17:38:40 +0800

    depth: 降低白方第4步以及黑方最后3步一层搜索

--- src/ai/search.cpp
+++ src/ai/search.cpp
@@ -70,8 +70,8 @@ depth_t MillGameAi_ab::changeDepth(depth_t originalDepth)
 #ifdef RAPID_GAME
         depth_t depthTable[] = { 6, 14, 15, 16, 15, 15, 15, 13, 10,  9, 8, 7, 1 };
 #else
-        depth_t depthTable[] = { 6, 15, 16, 17, 16, 16, 16, 14, 13, 12, 9, 7, 1 };
-      //depth_t depthTable[] = { 6, 15, 16, 17, 16, 16, 16, 12, 12, 12, 9, 7, 1 };
+        depth_t depthTable[] = { 6, 14, 15, 16, 16, 16, 16, 14, 12, 12, 9, 7, 1 };
+      //depth_t depthTable[] = { 6, 15, 16, 17, 16, 16, 16, 14, 13, 12, 9, 7, 1 };
 #endif  // RAPID_GAME
 #else // TRANSPOSITION_TABLE_ENABLE
         depth_t depthTable[] = { 2, 13, 13, 13, 12, 11, 10,  9,  9,  8, 8, 7, 1 };
