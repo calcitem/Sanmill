e90266d3e1c6d3e4b87f975ad1b276459b699453
Calcitem <calcitem@outlook.com>
2019-11-03 13:00:45 +0800

    license: Change Licence to GPL 3.0
    由 LGPL 协议切换为 GPL 协议.

--- src/base/misc.cpp
+++ src/base/misc.cpp
@@ -1,24 +1,21 @@
-﻿/*****************************************************************************
- * Copyright (C) 2018-2019 MillGame authors
- *
- * Authors: liuweilhy <liuweilhy@163.com>
- *          Calcitem <calcitem@outlook.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
- *****************************************************************************/
+﻿/*
+  Sanmill, a mill game playing engine derived from NineChess 1.5
+  Copyright (C) 2015-2018 liuweilhy (NineChess author)
+  Copyright (C) 2019 Calcitem <calcitem@outlook.com>
+
+  Sanmill is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Sanmill is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "config.h"
 #include "misc.h"
-
