e22f189f67dcc23126e4e4d09f6b49a8e399a4f1
Calcitem <calcitem@outlook.com>
2020-03-21 02:38:38 +0800

    ai: 如果结点有孩子就不重复生成

--- src/ai/search.cpp
+++ src/ai/search.cpp
@@ -839,16 +839,18 @@ value_t AIAlgorithm::search(depth_t depth, value_t alpha, value_t beta, Node *no
     }
 
     // 生成子节点树，即生成每个合理的着法
-    int moveSize = st->generateMoves(moves);
+    if (node->childrenSize == 0) {
+        int moveSize = st->generateMoves(moves);
 
-    st->generateChildren(moves, this, node
+        st->generateChildren(moves, this, node
 #ifdef BEST_MOVE_ENABLE
                              , bestMove
 #endif // BEST_MOVE_ENABLE
-                              );
+                             );
 
-    if (node == root && moveSize == 1) {
-        return node->value;
+        if (node == root && moveSize == 1) {
+            return node->value;
+        }
     }
 
     // 排序子节点树
