2cf96079ccc2ea56821a6153e80c324917a78367
Calcitem <calcitem@outlook.com>
2021-02-21 01:44:42 +0800

    monkey.sh: cd to adb path first (Windows)

--- tests/monkey.sh
+++ tests/monkey.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-adb shell monkey -v -p com.calcitem.sanmill --pct-touch 100 --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-syskeys 0 --pct-anyevent 0 1000
+cd ~/AppData/Local/Android/Sdk/platform-tools
+./adb shell monkey -v -p com.calcitem.sanmill --pct-touch 100 --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-syskeys 0 --pct-anyevent 0 1000
