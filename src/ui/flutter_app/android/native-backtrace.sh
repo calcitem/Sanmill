6e1d7f83197e233cf4510bc30db02bf71844baa5
Calcitem <calcitem@outlook.com>
2021-04-10 11:58:12 +0800

    flutter: android: Add armeabi-v7a to native-backtrace.sh

--- src/ui/flutter_app/android/native-backtrace.sh
+++ src/ui/flutter_app/android/native-backtrace.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 "${ANDROID_NDK_HOME}/ndk-stack.cmd" -sym "../build/app/intermediates/cmake/debug/obj/arm64-v8a" -dump bt.txt
+"${ANDROID_NDK_HOME}/ndk-stack.cmd" -sym "../build/app/intermediates/cmake/debug/obj/armeabi-v7a" -dump bt.txt
