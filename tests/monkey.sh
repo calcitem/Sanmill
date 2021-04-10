1339324368678368e246988c573943ff3636e81e
Calcitem <calcitem@outlook.com>
2021-04-10 15:29:31 +0800

    flutter: android: monkey: Change throttle from 100 to 500

--- tests/monkey.sh
+++ tests/monkey.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ~/AppData/Local/Android/Sdk/platform-tools || exit
-./adb shell monkey -v -p com.calcitem.sanmill --pct-touch 100 --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-syskeys 0 --pct-anyevent 0 --throttle 100 10000000
+./adb shell monkey -v -p com.calcitem.sanmill --pct-touch 100 --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-syskeys 0 --pct-anyevent 0 --throttle 500 10000000
