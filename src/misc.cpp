074795676f9ee4f5e8e7da7503e1c3c05bf240c5
Calcitem <calcitem@outlook.com>
2021-05-11 23:32:39 +0800

    misc: Remove _WIN32 code

--- src/misc.cpp
+++ src/misc.cpp
@@ -16,29 +16,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifdef _WIN32
-#if _WIN32_WINNT < 0x0601
-#undef  _WIN32_WINNT
-#define _WIN32_WINNT 0x0601 // Force to include needed API prototypes
-#endif
-
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-
-#include <windows.h>
-// The needed Windows API for processor groups could be missed from old Windows
-// versions, so instead of calling them directly (forcing the linker to resolve
-// the calls at compile time), try to load them at runtime. To do this we need
-// first to define the corresponding function pointers.
-extern "C" {
-    typedef bool(*fun1_t)(LOGICAL_PROCESSOR_RELATIONSHIP,
-                          PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX, PDWORD);
-    typedef bool(*fun2_t)(USHORT, PGROUP_AFFINITY);
-    typedef bool(*fun3_t)(HANDLE, CONST GROUP_AFFINITY *, PGROUP_AFFINITY);
-}
-#endif
-
 #include <fstream>
 #include <iomanip>
 #include <iostream>
