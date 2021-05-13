2852dbf1f7e74ef805b78e89e7ed4b94a59c0b15
Calcitem <calcitem@outlook.com>
2021-05-13 22:13:50 +0800

    flutter: windows: Change title to "Mill (N Men's Morris)"

--- src/ui/flutter_app/windows/runner/main.cpp
+++ src/ui/flutter_app/windows/runner/main.cpp
@@ -30,7 +30,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(428, 926);
-  if (!window.CreateAndShow(L"Mill Game", origin, size)) {
+  if (!window.CreateAndShow(L"Mill (N Men's Morris)", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
