ca5ed909e540d1ef44508ece99137422b3b87d17
Calcitem <calcitem@outlook.com>
2021-07-03 23:51:41 +0800

    Revert "icon: Add circle to person icon in main menu"
    This reverts commit 9fd239b93afc0e4cabc6053a5ab09767c2a53cbf.

--- src/ui/flutter_app/lib/widgets/home_drawer.dart
+++ src/ui/flutter_app/lib/widgets/home_drawer.dart
@@ -78,12 +78,12 @@ class _HomeDrawerState extends State<HomeDrawer> {
       DrawerListItem(
         index: DrawerIndex.humanVsAi,
         title: S.of(context).humanVsAi,
-        icon: Icon(Icons.account_circle_outlined),
+        icon: Icon(Icons.person_outline),
       ),
       DrawerListItem(
         index: DrawerIndex.humanVsHuman,
         title: S.of(context).humanVsHuman,
-        icon: Icon(Icons.supervised_user_circle_outlined),
+        icon: Icon(Icons.people_outline),
       ),
       DrawerListItem(
         index: DrawerIndex.aiVsAi,
