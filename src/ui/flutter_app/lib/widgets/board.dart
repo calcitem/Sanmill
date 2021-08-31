8b05014d52203a184953abd106122e38ebaa3578
Calcitem <calcitem@outlook.com>
2021-08-31 23:24:02 +0800

    accessibility: Read coordinates first and then read piece name

--- src/ui/flutter_app/lib/widgets/board.dart
+++ src/ui/flutter_app/lib/widgets/board.dart
@@ -271,7 +271,7 @@ class Board extends StatelessWidget {
     squareDesc.clear();
 
     for (var i = 0; i < 7 * 7; i++) {
-      squareDesc.add(pieceDesc[map[i] - 1] + ": " + coordinates[i]);
+      squareDesc.add(coordinates[i] + ": " + pieceDesc[map[i] - 1]);
     }
   }
 }
