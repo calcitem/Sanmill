e5193b83a1df603a635ab764d2f5f45c8f526a32
Calcitem <calcitem@outlook.com>
2021-09-05 11:07:35 +0800

    settings: Move experiments to developer mode card

--- src/ui/flutter_app/lib/widgets/game_settings_page.dart
+++ src/ui/flutter_app/lib/widgets/game_settings_page.dart
@@ -295,19 +295,6 @@ class _GameSettingsPageState extends State<GameSettingsPage> {
         ],
       ),
       SizedBox(height: AppTheme.sizedBoxHeight),
-      Text(S.of(context).experiments, style: AppTheme.settingsHeaderStyle),
-      SettingsCard(
-        context: context,
-        children: <Widget>[
-          SettingsSwitchListTile(
-            context: context,
-            value: Config.experimentsEnabled,
-            onChanged: setExperimentsEnabled,
-            titleString: S.of(context).experiments,
-          ),
-        ],
-      ),
-      SizedBox(height: AppTheme.sizedBoxHeight),
       Text(S.of(context).restore, style: AppTheme.settingsHeaderStyle),
       SettingsCard(
         context: context,
@@ -334,6 +321,13 @@ class _GameSettingsPageState extends State<GameSettingsPage> {
                   value: Config.developerMode,
                   onChanged: setDeveloperMode,
                   titleString: S.of(context).developerMode,
+                ),
+                ListItemDivider(),
+                SettingsSwitchListTile(
+                  context: context,
+                  value: Config.experimentsEnabled,
+                  onChanged: setExperimentsEnabled,
+                  titleString: S.of(context).experiments,
                 ),
                 ListItemDivider(),
                 SettingsSwitchListTile(
