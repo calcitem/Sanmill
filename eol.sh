227b4d03fe0a0cbc04c459c2263169004ffb462b
Calcitem <calcitem@outlook.com>
2021-11-21 02:31:11 +0800

    Add eol.sh to change some file's EOL from DOS to Unix
    Currently, it only change some C++ files.

---
+++ eol.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+dos2unix include/*.h include/*.template src/*.h src/*.cpp src/test/*.h src/test*.cpp src/perfect/*.h src/perfect/*.cpp src/ui/qt/*.h src/ui/qt/*.cpp src/ui/flutter_app/command/*.h src/ui/flutter_app/command/*.cpp
+
