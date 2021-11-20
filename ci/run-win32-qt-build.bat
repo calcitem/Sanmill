857f03504d01260ace0dd44c086489da4e44e788
Calcitem <calcitem@outlook.com>
2021-11-21 02:56:31 +0800

    ci: qt: Add qt-on-windows.yml

--- ci/run-win32-qt-build.bat
+++ ci/run-win32-qt-build.bat
@@ -1,10 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" x86_amd64
 
-set QTDIR=C:\Qt\Qt5.13.0\5.13.0\msvc2017_64
+set QTDIR=Qt\5.15.2\msvc2019_64
 set PATH=%PATH%;%QTDIR%\bin;
 
 qmake
 nmake clean
 nmake
-
-pause
