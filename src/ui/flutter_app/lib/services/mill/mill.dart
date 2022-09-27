f125b0f417541a5343c50bb146e69fc11662dae6
Calcitem <calcitem@outlook.com>
2022-09-27 21:02:13 +0800

    load: Support file picker
    Change-Id: I465b442393244c24e7c4360648cee2caffd652ce

--- src/ui/flutter_app/lib/services/mill/mill.dart
+++ src/ui/flutter_app/lib/services/mill/mill.dart
@@ -20,6 +20,7 @@ library mill;
 import 'dart:async';
 import 'dart:io';
 
+import 'package:filesystem_picker/filesystem_picker.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
--- src/ui/flutter_app/lib/services/mill/src/save_load_service.dart
+++ src/ui/flutter_app/lib/services/mill/src/save_load_service.dart
@@ -24,13 +24,44 @@ class LoadService {
 
   const LoadService._();
 
+  static getFilenameByDateTime() {
+    DateTime dateTime = DateTime.now();
+    String filename = dateTime.toString();
+    filename =
+        filename.replaceAll(" ", "_").replaceAll(":", "-").replaceAll(".", "_");
+    return "/$filename.pgn";
+  }
+
   /// Retrieves the file path.
   static Future<String> getFilePath() async {
     Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
     String appDocumentsPath = appDocumentsDirectory.path;
-    String filePath = '$appDocumentsPath/movelist.txt';
+    String filePath = appDocumentsPath + getFilenameByDateTime();
 
     return filePath;
+  }
+
+  /// Picks file.
+  static Future<String?> pickFile(BuildContext context) async {
+    Directory dir = await getApplicationDocumentsDirectory();
+
+    String? result = await FilesystemPicker.openDialog(
+      context: context,
+      rootDirectory: dir,
+      rootName: "Game Files", // TODO: l10n
+      fsType: FilesystemType.file,
+      permissionText: "Access to the storage was not granted.", // TODO: l10n
+      showGoUp: true,
+      allowedExtensions: ['.pgn'],
+      fileTileSelectMode:
+          FileTileSelectMode.checkButton, //  TODO: whole tile is better.
+    );
+
+    if (result == null) {
+      return null;
+    }
+
+    return result;
   }
 
   /// Saves the game to the file.
@@ -51,19 +82,34 @@ class LoadService {
 
   /// Read the game from the file.
   static Future<void> loadGame(BuildContext context) async {
-    Navigator.pop(context);
     rootScaffoldMessengerKey.currentState!.clearSnackBars();
 
-    File file = File(await getFilePath());
-    String fileContent = await file.readAsString();
+    String? result = await pickFile(context);
+    if (result == null) return;
+
+    File file = File(result);
+
+    late String fileContent;
+
+    try {
+      fileContent = await file.readAsString();
+    } catch (exception) {
+      MillController()
+          .headerTipNotifier
+          .showTip("Load failed!", snackBar: true); // TODO: l10n
+      Navigator.pop(context);
+      return;
+    }
 
     logger.v('File Content: $fileContent');
 
     try {
-      import(fileContent); // MillController().newRecorder = newHistory;
+      ImportService.import(
+          fileContent); // MillController().newRecorder = newHistory;
     } catch (exception) {
       final tip = S.of(context).cannotImport(fileContent);
       MillController().headerTipNotifier.showTip(tip, snackBar: true);
+      Navigator.pop(context);
       return;
     }
 
@@ -80,248 +126,7 @@ class LoadService {
       MillController().headerTipNotifier.showTip(tip, snackBar: true);
       HistoryNavigator.importFailedStr = "";
     }
-  }
 
-  static String _wmdNotationToMoveString(String wmd) {
-    if (wmd.length == 3 && wmd[0] == "x") {
-      if (wmdNotationToMove[wmd.substring(1, 3)] != null) {
-        return "-${wmdNotationToMove[wmd.substring(1, 3)]!}";
-      }
-    } else if (wmd.length == 2) {
-      if (wmdNotationToMove[wmd] != null) {
-        return wmdNotationToMove[wmd]!;
-      }
-    } else if (wmd.length == 5 && wmd[2] == "-") {
-      if (wmdNotationToMove[(wmd.substring(0, 2))] != null &&
-          wmdNotationToMove[(wmd.substring(3, 5))] != null) {
-        return "${wmdNotationToMove[(wmd.substring(0, 2))]!}->${wmdNotationToMove[(wmd.substring(3, 5))]!}";
-      }
-    } else if ((wmd.length == 8 && wmd[2] == "-" && wmd[5] == "x") ||
-        (wmd.length == 5 && wmd[2] == "x")) {
-      logger.w("$_tag Not support parsing format oo-ooxo notation.");
-      throw ImportFormatException(wmd);
-    }
-    throw ImportFormatException(wmd);
-  }
-
-  static String _playOkNotationToMoveString(String playOk) {
-    if (playOk.isEmpty) throw ImportFormatException(playOk);
-
-    final iDash = playOk.indexOf("-");
-    final iX = playOk.indexOf("x");
-
-    if (iDash == -1 && iX == -1) {
-      // 12
-      final val = int.parse(playOk);
-      if (val >= 1 && val <= 24) {
-        return playOkNotationToMove[playOk]!;
-      } else {
-        throw ImportFormatException(playOk);
-      }
-    }
-
-    if (iX == 0) {
-      // x12
-      final sub = playOk.substring(1);
-      final val = int.parse(sub);
-      if (val >= 1 && val <= 24) {
-        return "-${playOkNotationToMove[sub]!}";
-      } else {
-        throw ImportFormatException(playOk);
-      }
-    }
-    if (iDash != -1 && iX == -1) {
-      String? move;
-      // 12-13
-      final sub1 = playOk.substring(0, iDash);
-      final val1 = int.parse(sub1);
-      if (val1 >= 1 && val1 <= 24) {
-        move = playOkNotationToMove[sub1];
-      } else {
-        throw ImportFormatException(playOk);
-      }
-
-      final sub2 = playOk.substring(iDash + 1);
-      final val2 = int.parse(sub2);
-      if (val2 >= 1 && val2 <= 24) {
-        return "$move->${playOkNotationToMove[sub2]!}";
-      } else {
-        throw ImportFormatException(playOk);
-      }
-    }
-
-    logger.w("$_tag Not support parsing format oo-ooxo PlayOK notation.");
-    throw ImportFormatException(playOk);
-  }
-
-  static bool _isDalmaxMoveList(String text) {
-    if (text.length >= 15 && text.substring(0, 14) == '[Event "Dalmax') {
-      return true;
-    }
-
-    return false;
-  }
-
-  static bool _isPlayOkMoveList(String text) {
-    if (text.length >= 4 &&
-        text.substring(0, 3) == "1. " &&
-        int.tryParse(text.substring(3, 4)) != null) {
-      return true;
-    }
-
-    if (text.isNotEmpty && text[0] == "[") {
-      return true;
-    }
-
-    return false;
-  }
-
-  static bool _isGoldTokenMoveList(String text) {
-    if (text.length >= 10 &&
-        (text.substring(0, 9) == "GoldToken" ||
-            text.substring(0, 10) == "Past Moves" ||
-            text.substring(0, 5) == "Go to" ||
-            text.substring(0, 4) == "Turn" ||
-            text.substring(0, 8) == "(Player ")) {
-      return true;
-    }
-
-    return false;
-  }
-
-  @visibleForTesting
-  static void import(String moveList) {
-    var ml = moveList;
-
-    logger.v("Clipboard text: $moveList");
-
-    if (_isDalmaxMoveList(moveList)) {
-      ml = moveList.substring(moveList.indexOf("1. "));
-    }
-
-    if (_isPlayOkMoveList(moveList)) {
-      return _importPlayOk(moveList);
-    }
-
-    if (_isGoldTokenMoveList(moveList)) {
-      int start = moveList.indexOf("1\t");
-
-      if (start == -1) {
-        start = moveList.indexOf("1 ");
-      }
-
-      if (start == -1) {
-        start = 0;
-      }
-
-      ml = moveList.substring(start);
-    }
-
-    final GameRecorder newHistory =
-        GameRecorder(lastPositionWithRemove: MillController().position._fen);
-    final List<String> list = ml
-        .toLowerCase()
-        .replaceAll("\n", " ")
-        .replaceAll(",", " ")
-        .replaceAll(";", " ")
-        .replaceAll("!", " ")
-        .replaceAll("?", " ")
-        .replaceAll("#", " ")
-        .replaceAll("()", " ")
-        .replaceAll("white", " ")
-        .replaceAll("black", " ")
-        .replaceAll("win", " ")
-        .replaceAll("lose", " ")
-        .replaceAll("draw", " ")
-        .replaceAll("resign", " ")
-        .replaceAll("-/x", "x")
-        .replaceAll("/x", "x")
-        .replaceAll(".a", ". a")
-        .replaceAll(".b", ". b")
-        .replaceAll(".c", ". c")
-        .replaceAll(".d", ". d")
-        .replaceAll(".e", ". e")
-        .replaceAll(".f", ". f")
-        .replaceAll(".g", ". g")
-        // GoldToken
-        .replaceAll("\t", " ")
-        .replaceAll("place to ", "")
-        .replaceAll("  take ", "x")
-        .replaceAll(" -> ", "-")
-        // Finally
-        .split(" ");
-
-    for (var i in list) {
-      i = i.trim();
-
-      if (int.tryParse(i) != null) {
-        i = "$i.";
-      }
-
-      // TODO: [Leptopoda] Deduplicate
-      if (i.isNotEmpty && !i.endsWith(".")) {
-        if (i.length == 5 && i[2] == "x") {
-          // "a1xc3"
-          final String m1 = _wmdNotationToMoveString(i.substring(0, 2));
-          newHistory.add(ExtMove(m1));
-
-          final String m2 = _wmdNotationToMoveString(i.substring(2));
-          newHistory.add(ExtMove(m2));
-        } else if (i.length == 8 && i[2] == "-" && i[5] == "x") {
-          // "a1-b2xc3"
-          final String m1 = _wmdNotationToMoveString(i.substring(0, 5));
-          newHistory.add(ExtMove(m1));
-
-          final String m2 = _wmdNotationToMoveString(i.substring(5));
-          newHistory.add(ExtMove(m2));
-        } else {
-          // no x
-          final String m = _wmdNotationToMoveString(i);
-          newHistory.add(ExtMove(m));
-        }
-      }
-    }
-
-    if (newHistory.isNotEmpty) {
-      MillController().newRecorder = newHistory;
-    }
-  }
-
-  static void _importPlayOk(String moveList) {
-    final GameRecorder newHistory =
-        GameRecorder(lastPositionWithRemove: MillController().position._fen);
-
-    final List<String> list = moveList
-        .replaceAll("\n", " ")
-        .replaceAll(" 1/2-1/2", "")
-        .replaceAll(" 1-0", "")
-        .replaceAll(" 0-1", "")
-        .replaceAll("TXT", "")
-        .split(" ");
-
-    for (var i in list) {
-      i = i.trim();
-
-      if (i.isNotEmpty &&
-          !i.endsWith(".") &&
-          !i.startsWith("[") &&
-          !i.endsWith("]")) {
-        final iX = i.indexOf("x");
-        if (iX == -1) {
-          final String m = _playOkNotationToMoveString(i);
-          newHistory.add(ExtMove(m));
-        } else if (iX != -1) {
-          final String m1 = _playOkNotationToMoveString(i.substring(0, iX));
-          newHistory.add(ExtMove(m1));
-
-          final String m2 = _playOkNotationToMoveString(i.substring(iX));
-          newHistory.add(ExtMove(m2));
-        }
-      }
-    }
-
-    if (newHistory.isNotEmpty) {
-      MillController().newRecorder = newHistory;
-    }
+    Navigator.pop(context);
   }
 }
