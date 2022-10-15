d9da67f721c5124bbc95a781798f580894acb271
Calcitem <calcitem@outlook.com>
2022-10-16 01:36:38 +0800

    monkey: Add 50% motion event
    Change-Id: I4521f634337fc6d2173cb41935e3f4853b85f901

--- tests/monkey/monkey.sh
+++ tests/monkey/monkey.sh
@@ -9,4 +9,4 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 fi
 
 cd ${PLATFORM_TOOLS} || exit
-./adb shell monkey -v -p com.calcitem.sanmill --pct-touch 100 --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-syskeys 0 --pct-anyevent 0 --throttle 500 10000000
+./adb shell monkey -v -p com.calcitem.sanmill --pct-touch 50 --pct-motion 50 --pct-trackball 0 --pct-nav 0 --pct-majornav 0 --pct-syskeys 0 --pct-anyevent 0 --throttle 500 10000000
