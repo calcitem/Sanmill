e298898ff7ab98db49551c7781766d9ec075f1be
xshiwei <1105943292@qq.com>
2022-10-22 11:48:07 +0800

    Adjust UI details

--- src/ui/flutter_app/lib/shared/text_size_helper.dart
+++ src/ui/flutter_app/lib/shared/text_size_helper.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 class TextSizeHelper {
   // Get the size of the text through textPainter
-  static Size boundingTextSize(String text, TextStyle style,  {int maxLines = 2^31, double maxWidth = double.infinity}) {
+  static Size boundingTextSize(String text, TextStyle style,
+      {int maxLines = 2 ^ 31, double maxWidth = double.infinity}) {
     if (text.isEmpty) {
       return Size.zero;
     }
     final TextPainter textPainter = TextPainter(
         textDirection: TextDirection.ltr,
-        text: TextSpan(text: text, style: style), maxLines: maxLines)
+        text: TextSpan(text: text, style: style),
+        maxLines: maxLines)
       ..layout(maxWidth: maxWidth);
     return textPainter.size;
   }
