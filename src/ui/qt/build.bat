4df02e425a727259b492de8fe4bd6d8fa3e4b0a6
Calcitem <calcitem@outlook.com>
2022-12-17 20:23:16 +0800

    qt: cmake: Add windeployqt to build.bat and build release too
    Change-Id: I3645d7f28fa346d5639e53c95012f737bccc8f8b

--- src/ui/qt/build.bat
+++ src/ui/qt/build.bat
@@ -1,4 +1,9 @@
 git clean -fdx
 
 cmake .
+
 cmake --build . --target MillGame
+windeployqt "Debug\MillGame.exe
+
+cmake --build . --target MillGame --config Release
+windeployqt "Release\MillGame.exe
