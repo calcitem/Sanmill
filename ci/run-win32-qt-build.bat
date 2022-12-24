ee0c830429f410f851fff83bbf1608c0163b4261
Calcitem <calcitem@outlook.com>
2022-12-24 20:13:05 +0800

    Remove Some IDE files and modify CI for Qt
    Because we use cmake to build.
    
    Change-Id: Ia657886a1619c881c1254fb702289d3183716d3c

--- ci/run-win32-qt-build.bat
+++
@@ -1,8 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" x86_amd64
-
-set QTDIR=Qt\5.15.2\msvc2019_64
-set PATH=%PATH%;%QTDIR%\bin;
-
-qmake
-nmake clean
-nmake
