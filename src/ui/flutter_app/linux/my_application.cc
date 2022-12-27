44663d7635ccc2e0dd807e65cebab7ff7727a90e
Calcitem <calcitem@outlook.com>
2022-12-27 21:37:30 +0800

    linux: Refacotr my_application.cc
    Change-Id: Ie00d93c12854bef2658ed093b018134a7202a9fa

--- src/ui/flutter_app/linux/my_application.cc
+++ src/ui/flutter_app/linux/my_application.cc
@@ -117,17 +117,18 @@ static void my_application_activate(GApplication *application)
 #if 1
     if (engine == nullptr) {
         engine = new MillEngine();
-    }
 
-    // Get engine from view
-    FlEngine *fl_engine = fl_view_get_engine(view);
+        // Get engine from view
+        FlEngine *fl_engine = fl_view_get_engine(view);
 
-    g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
-    g_autoptr(FlBinaryMessenger) messenger = fl_engine_get_binary_messenger(fl_engine);
-    g_autoptr(FlMethodChannel) channel = fl_method_channel_new(messenger,
+        g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
+        g_autoptr(FlBinaryMessenger) messenger = fl_engine_get_binary_messenger(fl_engine);
+        g_autoptr(FlMethodChannel) channel = fl_method_channel_new(messenger,
                                         "com.calcitem.sanmill/engine",
                                         FL_METHOD_CODEC(codec));
-    fl_method_channel_set_method_call_handler(channel, method_call_cb, g_object_ref(view), g_object_unref);
+        fl_method_channel_set_method_call_handler(channel, method_call_cb, g_object_ref(view), g_object_unref);
+    }
+
 #endif
      // END OF OUR CUSTOM BLOCK
 
