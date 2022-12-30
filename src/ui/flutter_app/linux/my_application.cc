eeb9083f9f444a1741366c8155d05ef9bd5c0b00
Calcitem <calcitem@outlook.com>
2022-12-30 23:41:47 +0800

    Linux: Do not show maximize button
    Change-Id: Idbd2a25b3dbd199b4bc29f1586b54945d5832d95

--- src/ui/flutter_app/linux/my_application.cc
+++ src/ui/flutter_app/linux/my_application.cc
@@ -89,6 +89,7 @@ static void my_application_activate(GApplication *application)
     }
 
     gtk_window_set_default_size(window, 428, 926);
+    gtk_window_set_resizable(GTK_WINDOW (window), FALSE);
     gtk_widget_show(GTK_WIDGET(window));
 
     g_autoptr(FlDartProject) project = fl_dart_project_new();
