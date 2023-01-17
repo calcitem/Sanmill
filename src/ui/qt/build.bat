d4da13556fb2e17b3dd890efa6966eaa94da0b7a
Calcitem <calcitem@outlook.com>
2023-01-18 00:19:04 +0800

    qt: build.sh: Add cov-build command
    Change-Id: Ifc8e4ef0f0f26941ff3189ef990c82af2bdc2a9b

--- src/ui/qt/build.bat
+++ src/ui/qt/build.bat
@@ -7,3 +7,5 @@ windeployqt "Debug\mill-pro.exe
 
 cmake --build . --target mill-pro --config Release
 windeployqt "Release\mill-pro.exe
+
+# cov-build  --dir cov-int cmake --build . --target mill-pro
