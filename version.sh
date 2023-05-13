1acb058eb4dad8a6ddb76ed2d2b8864d318fef10
Calcitem <calcitem@outlook.com>
2023-05-13 17:15:50 +0800

    Add comments to version.sh
    Change-Id: Id6d8c51f269d35ae3abc82c42323044ca4845078

--- version.sh
+++ version.sh
@@ -5,19 +5,25 @@ TEMPLATE_FILE=include/version.h.template
 PUBSPEC_YAML_FILE=src/ui/flutter_app/pubspec.yaml
 GIT_BRANCH=master
 
+# Define sed command, use gsed on macOS
 SED=sed
-
 if [ "$(uname)" == "Darwin" ]; then
 	SED=gsed
 fi
 
+# Remove existing VERSION_H file
 rm -f $VERSION_H
 
+# Create a file with the sorted git commit hashes
 git rev-list HEAD | sort > config.git-hash
 
+# Calculate the number of commits in the repository
 LOCALVER="$(wc -l config.git-hash | awk '{print $1}')"
+
+# Get the latest git tag
 TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
 
+# Determine the version string based on the number of commits
 if [ "$LOCALVER" -gt "1" ] ; then
 	VER=$(git rev-list origin/$GIT_BRANCH | sort | join config.git-hash - | wc -l | awk '{print $1}')
 	if [ "$VER" != "$LOCALVER" ] ; then
@@ -41,17 +47,22 @@ else
 	APP_VERSION="${TAG:1}+${VER}"
 fi
 
+# Remove the temporary git-hash file
 rm -f config.git-hash
 
+# Replace the version placeholder in the template file and create VERSION_H
 $SED "s/\$FULL_VERSION/$GIT_VERSION/g" < $TEMPLATE_FILE > $VERSION_H
 
+# Tell git to ignore changes in VERSION_H
 git update-index --assume-unchanged $VERSION_H
 
+# Print the generated version string
 echo "App Version: ${APP_VERSION}"
 echo
 echo "Generated $VERSION_H"
 echo
 cat $VERSION_H
 
+# Remove the version line from the pubspec.yaml file and insert the new version
 $SED -i '/version:/d' ${PUBSPEC_YAML_FILE}
 $SED -i "4i\version: ${APP_VERSION}" ${PUBSPEC_YAML_FILE}
