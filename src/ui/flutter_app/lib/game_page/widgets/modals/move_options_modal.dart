calcitem/Sanmill

commit a4e9d58d1520d1cc1a1357afa748cce6509e7681

1
Change-Id: I795eb2d36af9518f7fbd0fcbd59ea443b53483b5

src/ui/flutter_app/lib/game_page/widgets/modals/move_options_modal.dart
@@ -22,12 +22,13 @@ class _MoveOptionsModal extends StatelessWidget {
   final BuildContext mainContext;
 
   void _showMoveList(BuildContext context) {
-    Navigator.pop(context);
     showDialog(
       context: context,
       barrierDismissible: true,
       builder: (_) => const _MoveListDialog(),
     );
+
+    Navigator.pop(context);
   }
 
   @override

src/ui/flutter_app/lib/appearance_settings/widgets/appearance_settings_page.dart
@@ -121,9 +121,9 @@ class AppearanceSettingsPage extends StatelessWidget {
 
   void _setTheme(BuildContext context, ColorSettings colorSettings) {
     void callback(ColorTheme? theme) {
-      Navigator.pop(context);
 
       if (theme == ColorTheme.current) {
+        Navigator.pop(context);
         return;
       }
 
@@ -156,6 +156,8 @@ class AppearanceSettingsPage extends StatelessWidget {
         onChanged: callback,
       ),
     );
+
+    Navigator.pop(context);
   }
 
   Widget _buildColorSettings(BuildContext context, Box<ColorSettings> box, _) {

src/ui/flutter_app/lib/game_page/services/controller/history_navigation.dart
@@ -39,13 +39,12 @@ class HistoryNavigator {
   }) async {
     assert(navMode != HistoryNavMode.takeBackN || number != null);
 
-    if (pop) {
-      Navigator.pop(context);
-    }
-
     if (GameController().isEngineInDelay == true) {
       rootScaffoldMessengerKey.currentState!
           .showSnackBarClear(S.of(context).aiIsDelaying);
+      if (pop) {
+        Navigator.pop(context);
+      }
       return const HistoryOK();
     }
 
@@ -65,6 +64,10 @@ class HistoryNavigator {
       logger.i(
         "$_logTag Is going to history, ignore Take Back button press.",
       );
+
+      if (pop) {
+        Navigator.pop(context);
+      }
 
       return const HistoryOK();
     }
@@ -104,6 +107,10 @@ class HistoryNavigator {
     await navMode.gotoHistoryPlaySound();
 
     _isGoingToHistory = false;
+
+    if (pop) {
+      Navigator.pop(context);
+    }
 
     return resp;
   }

src/ui/flutter_app/lib/game_page/services/import_export/import_export_service.dart
@@ -27,24 +27,24 @@ class ImportService {
 
   /// Exports the game to the devices clipboard.
   static Future<void> exportGame(BuildContext context) async {
-    Navigator.pop(context);
-
     await Clipboard.setData(
       ClipboardData(text: GameController().gameRecorder.moveHistoryText),
     );
 
     rootScaffoldMessengerKey.currentState!
         .showSnackBarClear(S.of(context).moveHistoryCopied);
+
+    Navigator.pop(context);
   }
 
   /// Tries to import the game saved in the device's clipboard.
   static Future<void> importGame(BuildContext context) async {
-    Navigator.pop(context);
     rootScaffoldMessengerKey.currentState!.clearSnackBars();
 
     final ClipboardData? data = await Clipboard.getData(Clipboard.kTextPlain);
 
     if (data?.text == null) {
+      Navigator.pop(context);
       return;
     }
 
@@ -54,6 +54,7 @@ class ImportService {
       final String tip = S.of(context).cannotImport(data!.text!);
       GameController().headerTipNotifier.showTip(tip);
       //MillController().animationController.forward();
+      Navigator.pop(context);
       return;
     }
 
@@ -68,6 +69,8 @@ class ImportService {
       GameController().headerTipNotifier.showTip(tip);
       HistoryNavigator.importFailedStr = "";
     }
+
+    Navigator.pop(context);
   }
 
   static String _wmdNotationToMoveString(String wmd) {

src/ui/flutter_app/lib/game_page/services/save_load/save_load_service.dart
@@ -75,16 +75,16 @@ class LoadService {
 
     final String strGameSavedTo = S.of(context).gameSavedTo;
 
-    Navigator.pop(context);
-
     if (!(GameController().gameRecorder.hasPrevious == true ||
         GameController().isPositionSetup == true)) {
+      Navigator.pop(context);
       return;
     }
 
     final String? filename = await getFilePath(context);
 
     if (filename == null) {
+      Navigator.pop(context);
       return;
     }
 
@@ -95,6 +95,8 @@ class LoadService {
 
     rootScaffoldMessengerKey.currentState!
         .showSnackBarClear("$strGameSavedTo $filename");
+
+    Navigator.pop(context);
   }
 
   /// Read the game from the file.

src/ui/flutter_app/lib/game_page/widgets/dialogs/game_result_alert_dialog.dart
@@ -83,12 +83,12 @@ class GameResultAlertDialog extends StatelessWidget {
               "[config] skillLevel: ${DB().generalSettings.skillLevel}",
             );
 
-            Navigator.pop(context);
             GameController().reset(force: true);
             GameController()
                 .headerTipNotifier
                 .showTip(S.of(context).gameStarted);
             GameController().headerIconsNotifier.showIcons();
+            Navigator.pop(context);
           },
         ),
         TextButton(
@@ -97,12 +97,12 @@ class GameResultAlertDialog extends StatelessWidget {
             textScaleFactor: DB().displaySettings.fontScale,
           ),
           onPressed: () {
-            Navigator.pop(context);
             GameController().reset(force: true);
             GameController()
                 .headerTipNotifier
                 .showTip(S.of(context).gameStarted);
             GameController().headerIconsNotifier.showIcons();
+            Navigator.pop(context);
           },
         ),
         TextButton(
@@ -121,12 +121,12 @@ class GameResultAlertDialog extends StatelessWidget {
             textScaleFactor: DB().displaySettings.fontScale,
           ),
           onPressed: () {
-            Navigator.pop(context);
             GameController().reset(force: true);
             GameController()
                 .headerTipNotifier
                 .showTip(S.of(context).gameStarted);
             GameController().headerIconsNotifier.showIcons();
+            Navigator.pop(context);
           },
         ),
         TextButton(

src/ui/flutter_app/lib/game_page/widgets/dialogs/info_dialog.dart
@@ -175,15 +175,15 @@ class _InfoDialog extends StatelessWidget {
                     textScaleFactor: DB().displaySettings.fontScale,
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-
                     Clipboard.setData(
                       ClipboardData(text: content),
                     );
 
                     rootScaffoldMessengerKey.currentState!
                         .showSnackBarClear(S.of(context).done);
+
+                    Navigator.pop(context);
+                    Navigator.pop(context);
                   },
                 );
 

src/ui/flutter_app/lib/game_page/widgets/toolbars/src/setup_position_toolbar.dart
@@ -448,7 +448,6 @@ class SetupPositionToolbarState extends State<SetupPositionToolbar> {
     void callback(int? placed) {
       newPlaced = placed!;
       updateSetupPositionPiecesCount();
-      Navigator.pop(context);
       setState(() {});
       GameController().headerTipNotifier.showTip(
           S.of(context).hasPlacedPieceCount(newPlaced),
@@ -457,6 +456,8 @@ class SetupPositionToolbarState extends State<SetupPositionToolbar> {
       rootScaffoldMessengerKey.currentState!.showSnackBar(CustomSnackBar(
           _infoText(context),
           duration: const Duration(seconds: 6)));
+
+      Navigator.pop(context);
     }
 
     if (newPhase != Phase.placing) {

commit 3f9375c8a3ad585270f25de86075ab6403f7466b

Fix sometimes clicking the Move List button does not respond
This problem is currently sometimes reproducible on Windows, but not on Android.

Add delay to fix _MoveListDialog display issue after closing _MoveOptionsModal

In this commit, we added a 100ms delay using `Future.delayed()` to ensure that
the `_MoveListDialog` is displayed correctly after closing the `_MoveOptionsModal`.
This delay prevents the `_MoveListDialog` from disappearing and allows the users
to see it as intended. The context passed to `showDialog` is now valid,
and the semi-transparent background effect is preserved.

(Bug introduced by commit a4e9d58d1520d1cc1a1357afa748cce6509e7681.)

src/ui/flutter_app/lib/game_page/widgets/modals/move_options_modal.dart
@@ -22,13 +22,13 @@ class _MoveOptionsModal extends StatelessWidget {
   final BuildContext mainContext;
 
   void _showMoveList(BuildContext context) {
-    showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (_) => const _MoveListDialog(),
-    );
-
     Navigator.pop(context);
+    Future<void>.delayed(const Duration(milliseconds: 100), () {
+      showDialog<void>(
+        context: context,
+        builder: (BuildContext context) => const _MoveListDialog(),
+      );
+    });
   }
 
   @override