f3be9483cd093d392ffb48f58ed68b429e1ecbcc
Calcitem <calcitem@outlook.com>
2023-10-03 14:27:54 +0800

    qt: Add install-qt-env.sh

---
+++ src/ui/qt/install-qt-env.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Function to detect the operating system
+detect_os() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo "ubuntu"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "macos"
+    else
+        echo "unknown"
+    fi
+}
+
+# Function to install qt on MacOS
+install_qt_macos() {
+    brew install cmake
+    brew install qt
+    brew install qt5
+    echo 'export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"' >> ~/.zshrc
+    source ~/.zshrc
+}
+
+# Function to install qt on Ubuntu
+install_qt_ubuntu() {
+    sudo apt-get update
+    sudo apt-get install -y qt5-default qtmultimedia5-dev qtcreator cmake
+}
+
+# Main function to install qt
+install_qt() {
+    os=$(detect_os)
+    if [[ "$os" == "macos" ]]; then
+        install_qt_macos
+    elif [[ "$os" == "ubuntu" ]]; then
+        install_qt_ubuntu
+    else
+        echo "Unsupported operating system. This script supports MacOS and Ubuntu only."
+        exit 1
+    fi
+}
+
+# Run the main function
+install_qt
