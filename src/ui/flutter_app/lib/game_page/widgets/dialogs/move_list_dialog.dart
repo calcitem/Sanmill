9715ea6093c68221b58e4cf40583fe518ba5a340
Calcitem <calcitem@outlook.com>
2023-12-25 19:30:37 +0800

    Change button layout in GamePageActionSheet from vertical to horizontal
    Wrap action buttons in a Row widget to display them in a horizontal
    line instead of the default vertical stack in AlertDialog actions.

--- src/ui/flutter_app/lib/game_page/widgets/dialogs/move_list_dialog.dart
+++ src/ui/flutter_app/lib/game_page/widgets/dialogs/move_list_dialog.dart
@@ -27,11 +27,22 @@ class _MoveListDialog extends StatelessWidget {
     final int end = controller.gameRecorder.length - 1;
 
     final TextStyle titleTextStyle =
-        Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: AppTheme.gamePageActionSheetTextColor,
-              fontSize: AppTheme.textScaler.scale(AppTheme.largeFontSize),
-            );
-    final TextStyle buttonTextStyle = titleTextStyle;
+    Theme
+        .of(context)
+        .textTheme
+        .titleLarge!
+        .copyWith(
+      color: AppTheme.gamePageActionSheetTextColor,
+      fontSize: AppTheme.textScaler.scale(AppTheme.largeFontSize),
+    );
+    final TextStyle buttonTextStyle = Theme
+        .of(context)
+        .textTheme
+        .titleMedium!
+        .copyWith(
+      color: AppTheme.gamePageActionSheetTextColor,
+      fontSize: AppTheme.textScaler.scale(AppTheme.defaultFontSize),
+    );
 
     if (DB().generalSettings.screenReaderSupport) {
       rootScaffoldMessengerKey.currentState!.clearSnackBars();
@@ -41,7 +52,9 @@ class _MoveListDialog extends StatelessWidget {
       child: AlertDialog(
         backgroundColor: UIColors.semiTransparentBlack,
         title: Text(
-          S.of(context).moveList,
+          S
+              .of(context)
+              .moveList,
           style: titleTextStyle.copyWith(
               fontSize: AppTheme.textScaler
                   .scale(titleTextStyle.fontSize ?? AppTheme.largeFontSize)),
@@ -50,7 +63,11 @@ class _MoveListDialog extends StatelessWidget {
           child: Text(
             moveHistoryText,
             textDirection: TextDirection.ltr,
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            style: Theme
+                .of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(
                 fontSize: AppTheme.textScaler
                     .scale(titleTextStyle.fontSize ?? AppTheme.largeFontSize),
                 color: AppTheme.gamePageActionSheetTextColor,
@@ -60,27 +77,38 @@ class _MoveListDialog extends StatelessWidget {
           ),
         ),
         actions: <Widget>[
-          if (end > 0)
-            TextButton(
-              child: Text(
-                S.of(context).rollback,
-                style: buttonTextStyle,
+          Row( // Wrap the buttons with a Row widget
+            mainAxisSize: MainAxisSize.min, // Use min to fit the content size
+            children: <Widget>[
+              if (end > 0)
+                TextButton(
+                  child: Text(
+                    S
+                        .of(context)
+                        .rollback,
+                    style: buttonTextStyle,
+                  ),
+                  onPressed: () async => _rollback(context, end),
+                ),
+              TextButton(
+                child: Text(
+                  S
+                      .of(context)
+                      .copy,
+                  style: buttonTextStyle,
+                ),
+                onPressed: () => GameController.export(context),
               ),
-              onPressed: () async => _rollback(context, end),
-            ),
-          TextButton(
-            child: Text(
-              S.of(context).copy,
-              style: buttonTextStyle,
-            ),
-            onPressed: () => GameController.export(context),
-          ),
-          TextButton(
-            child: Text(
-              S.of(context).cancel,
-              style: buttonTextStyle,
-            ),
-            onPressed: () => Navigator.pop(context),
+              TextButton(
+                child: Text(
+                  S
+                      .of(context)
+                      .cancel,
+                  style: buttonTextStyle,
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
           ),
         ],
       ),
