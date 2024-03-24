8ac2f91db078414b4ffcc5f952036b4f3c7fe31b
Calcitem <calcitem@outlook.com>
2024-03-24 14:00:40 +0800

    qt: build.sh: Add XCode project gen sample

--- src/ui/qt/build.sh
+++ src/ui/qt/build.sh
@@ -8,4 +8,7 @@ cmake --build . --target mill-pro
 cmake --build . --target mill-pro --config Release
 #windeployqt "Release/mill-pro"
 
+# cmake .. -G "Xcode" -DCMAKE_PREFIX_PATH=~/Qt5.12.12/5.12.12/clang_64 -DCMAKE_OSX_ARCHITECTURES=x86_64
+
 # cov-build  --dir cov-int cmake --build . --target mill-pro
+
