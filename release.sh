8f81beabb223a83dd18bed707b6e5c3452d0e609
Calcitem <calcitem@outlook.com>
2024-05-17 01:03:09 +0800

    version: Fix some file's version string

--- release.sh
+++ release.sh
@@ -99,7 +99,7 @@ $SED -i "s/${VERSION_STRING}/${NEW_VERSION_STRING}/g" $YAML_FILE
 $SED -i "s/version: ${OLD_VERSION}/version: ${NEW_VERSION}/g" $SNAP_YAML_FILE
 $SED -i "s/Version=${OLD_VERSION}/Version=${NEW_VERSION}/g" $SNAP_DESKTOP_FILE
 
-# Modify Debain
+# Modify Debian
 $SED -i "s/Version: ${OLD_VERSION}/Version: ${NEW_VERSION}/g" $DEBIAN_YAML_FILE
 $SED -i "s/Version=${OLD_VERSION}/Version=${NEW_VERSION}/g" $DEBIAN_DESKTOP_FILE
 
