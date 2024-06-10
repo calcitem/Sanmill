44bc80ef0a534043ca6ba6af532cb3551b03fda7
Calcitem <calcitem@outlook.com>
2024-06-10 11:18:02 +0800

    qt: Allow choose Qt5 in build.bat file

--- src/ui/qt/build.bat
+++ src/ui/qt/build.bat
@@ -1,15 +1,28 @@
 @echo off
 setlocal
 
+:: Define which version of Qt to use, possible values are "Qt5" or "Qt6"
+set "QtVersion=Qt6"
+
 :: Clean repository
 git clean -fdx
 
-:: Detect system architecture and set Qt6_DIR accordingly
-if exist "%ProgramFiles% (Arm)" (
-    set "Qt6_DIR=C:\Qt\6.7.1\msvc2019_arm64\lib\cmake\Qt6"
-) else (
-    set "Qt6_DIR=C:\Qt\6.7.1\msvc2019_64\lib\cmake\Qt6"
+:: Set Qt directories and system architecture depending on the chosen Qt version
+if "%QtVersion%"=="Qt6" (
+    if exist "%ProgramFiles% (Arm)" (
+        set "Qt_DIR=C:\Qt\6.7.1\msvc2019_arm64\lib\cmake\Qt6"
+        set "arch=ARM64"
+    ) else (
+        set "Qt_DIR=C:\Qt\6.7.1\msvc2019_64\lib\cmake\Qt6"
+        set "arch=X64"
+    )
+) else if "%QtVersion%"=="Qt5" (
+    set "Qt_DIR=C:\Qt\5.15.2\msvc2019_64\lib\cmake\Qt5"
+    set "arch=X64"
 )
+
+:: Set CMAKE_PREFIX_PATH to Qt_DIR
+set "CMAKE_PREFIX_PATH=%Qt_DIR%"
 
 :: Detect Visual Studio version
 set "vsver="
@@ -26,21 +39,21 @@ if not defined vsver (
     exit /b 1
 )
 
-:: Set system architecture based on previous detection
-if exist "%ProgramFiles% (Arm)" (
-    set "arch=ARM64"
-) else (
-    set "arch=X64"
-)
-
 :: Generate project files
-cmake -G "%vsver%" -A %arch% .
+cmake -G "%vsver%" -A %arch% -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH% .
 
 :: Build and deploy Debug version
 cmake --build . --target mill-pro --config Debug -j
-C:\Qt\Tools\QtDesignStudio\qt6_design_studio_reduced_version\bin\windeployqt "Debug\mill-pro.exe"
+if "%QtVersion%"=="Qt6" (
+    C:\Qt\Tools\QtDesignStudio\qt6_design_studio_reduced_version\bin\windeployqt "Debug\mill-pro.exe"
+) else (
+    C:\Qt\5.15.2\msvc2019_64\bin\windeployqt "Debug\mill-pro.exe"
+)
 
 :: Build and deploy Release version
 cmake --build . --target mill-pro --config Release -j
-C:\Qt\Tools\QtDesignStudio\qt6_design_studio_reduced_version\bin\windeployqt "Release\mill-pro.exe"
-
+if "%QtVersion%"=="Qt6" (
+    C:\Qt\Tools\QtDesignStudio\qt6_design_studio_reduced_version\bin\windeployqt "Release\mill-pro.exe"
+) else (
+    C:\Qt\5.15.2\msvc2019_64\bin\windeployqt "Release\mill-pro.exe"
+)
