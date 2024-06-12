c100ff6a91c391cd3526fd5a0c65656b791d4669
Calcitem <calcitem@outlook.com>
2024-06-13 00:29:02 +0800

    qt: Update Help URL

--- src/ui/qt/gamewindow.cpp
+++ src/ui/qt/gamewindow.cpp
@@ -1023,8 +1023,8 @@ void MillGameWindow::on_actionEngine_E_triggered()
 
 void MillGameWindow::on_actionViewHelp_V_triggered()
 {
-    QDesktopServices::openUrl(QUrl("https://github.com/calcitem/Sanmill/wiki/"
-                                   "Perfect-Database"));
+    QDesktopServices::openUrl(QUrl("https://github.com/calcitem/Sanmill/blob/"
+                                   "master/src/perfect/README.md"));
 }
 
 void MillGameWindow::on_actionWeb_W_triggered()
