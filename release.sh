ac9df461027eade56484abcc3d48bac0ed025bdf
Calcitem <calcitem@outlook.com>
2024-08-24 12:49:43 +0800

    release.sh: Fix some version cannot change

--- release.sh
+++ release.sh
@@ -57,23 +57,23 @@ fi
 ./version.sh
 
 # version: 1.0.6+1811
-VERSION_STRING=$($SED -n '4p' "$YAML_FILE")
+VERSION_STRING=`$SED -n '4p' $YAML_FILE`
 echo "VERSION_STRING = $VERSION_STRING"
 
 # 1.0.6+1811
-FULL_VERSION=$(echo "$VERSION_STRING" | cut -d ' ' -f 2)
+FULL_VERSION=`echo $VERSION_STRING | cut -d ' ' -f 2`
 echo "FULL_VERSION = $FULL_VERSION"
 
 # 1.0.6
-VERSION=$(echo "$FULL_VERSION" | cut -d "+" -f 1)
+VERSION=`echo $FULL_VERSION | cut -d "+" -f 1`
 echo "VERSION = $VERSION"
 OLD_VERSION=$VERSION
 echo "OLD_VERSION = $OLD_VERSION"
 
 # 6
-MAJOR_NUMBER=$(echo "$VERSION" | cut -d "." -f 1)
-MINOR_NUMBER=$(echo "$VERSION" | cut -d "." -f 2)
-PATCH_NUMBER=$(echo "$VERSION" | cut -d "." -f 3)
+MAJOR_NUMBER=`echo $VERSION | cut -d "." -f 1`
+MINOR_NUMBER=`echo $VERSION | cut -d "." -f 2`
+PATCH_NUMBER=`echo $VERSION | cut -d "." -f 3`
 
 echo "Old:"
 echo "MAJOR_NUMBER = $MAJOR_NUMBER"
@@ -108,7 +108,7 @@ NEW_VERSION="${MAJOR_NUMBER}.${MINOR_NUMBER}.${PATCH_NUMBER}"
 echo "NEW_VERSION = $NEW_VERSION"
 
 # 1811
-BUILD_NUMBER=$(echo "$FULL_VERSION" | cut -d "+" -f 2)
+BUILD_NUMBER=`echo $FULL_VERSION | cut -d "+" -f 2`
 echo "BUILD_NUMBER = $BUILD_NUMBER"
 
 # 1.0.7+1811
@@ -120,60 +120,60 @@ NEW_VERSION_STRING="version: $NEW_FULL_VERSION"
 echo "NEW_VERSION_STRING = $NEW_VERSION_STRING"
 
 # Modify yaml
-$SED -i "s/${VERSION_STRING}/${NEW_VERSION_STRING}/g" "$YAML_FILE"
+$SED -i "s/${VERSION_STRING}/${NEW_VERSION_STRING}/g" $YAML_FILE
 
 # Modify Snap
-$SED -i "s/version: ${OLD_VERSION}/version: ${NEW_VERSION}/g" "$SNAP_YAML_FILE"
-$SED -i "s/Version=${OLD_VERSION}/Version=${NEW_VERSION}/g" "$SNAP_DESKTOP_FILE"
+$SED -i "s/version: ${OLD_VERSION}/version: ${NEW_VERSION}/g" $SNAP_YAML_FILE
+$SED -i "s/Version=${OLD_VERSION}/Version=${NEW_VERSION}/g" $SNAP_DESKTOP_FILE
 
 # Modify Debian
-$SED -i "s/Version: ${OLD_VERSION}/Version: ${NEW_VERSION}/g" "$DEBIAN_YAML_FILE"
-$SED -i "s/Version=${OLD_VERSION}/Version=${NEW_VERSION}/g" "$DEBIAN_DESKTOP_FILE"
+$SED -i "s/Version: ${OLD_VERSION}/Version: ${NEW_VERSION}/g" $DEBIAN_YAML_FILE
+$SED -i "s/Version=${OLD_VERSION}/Version=${NEW_VERSION}/g" $DEBIAN_DESKTOP_FILE
 
 # Modify Qt
 OLD_FILEVERSION="$MAJOR_NUMBER,$MINOR_NUMBER,$OLD_PATCH_NUMBER"
 FILEVERSION="$MAJOR_NUMBER,$MINOR_NUMBER,$PATCH_NUMBER"
-$SED -i "s/${OLD_FILEVERSION},0/${FILEVERSION},0/g" "$QT_RC_FILE"
-$SED -i "s/${OLD_VERSION}.0/${NEW_VERSION}.0/g" "$QT_RC_FILE"
+$SED -i "s/${OLD_FILEVERSION},0/${FILEVERSION},0/g" $QT_RC_FILE
+$SED -i "s/${OLD_VERSION}.0/${NEW_VERSION}.0/g" $QT_RC_FILE
 
 # Changelog
-rm -f "${BUILD_NUMBER}.txt"
-touch "${BUILD_NUMBER}.txt"
+rm -f ${BUILD_NUMBER}.txt
+touch ${BUILD_NUMBER}.txt
 
-echo "v$NEW_VERSION" >> "${BUILD_NUMBER}.txt"
-echo >> "${BUILD_NUMBER}.txt"
+echo "v$NEW_VERSION" >> ${BUILD_NUMBER}.txt
+echo >> ${BUILD_NUMBER}.txt
 
 for DIR in "${CHANGELOG_DIRS[@]}"; do
-  mkdir -p "$DIR" || true
-  cp "${BUILD_NUMBER}.txt" "$DIR"
+  mkdir -p $DIR || true
+  cp ${BUILD_NUMBER}.txt $DIR
   case $DIR in
-    *ar*) echo "يتضمن هذا التحديث تحسينات وإصلاحات للأخطاء لجعل التطبيق أفضل بالنسبة لك." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *bg*) echo "Това актуализиране включва различни подобрения и корекции на грешки, за да направи приложението по-добро за вас." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *bo*) echo "ད་དུང་འདི་ལག་ཐོག་སྤུན་རྒྱུད་བཟོ་བཅོས་དང་བདག་སྤྱོད་གཏོང་མཁན་སོགས་ནང་འཁོད་དང་འབྲི་གཏོང་མཁན་སྔར་བཞིན་གཞན་དང་མཉམ་འབྲི་འདུག་གས།" >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *cs-CZ*) echo "Tato aktualizace obsahuje různá vylepšení a opravy chyb, aby byla aplikace pro vás lepší." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *de-DE*) echo "Dieses Update umfasst verschiedene Verbesserungen und Fehlerbehebungen, um die App für dich zu verbessern." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *en-US*) echo "This update includes various improvements and bug fixes to make the app better for you." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *es-ES*) echo "Esta actualización incluye varias mejoras y correcciones de errores para mejorar la aplicación para ti." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *fa-IR*) echo "این به‌روزرسانی شامل بهبودها و رفع اشکالات مختلفی است که برنامه را برای شما بهتر می‌کند." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *fr*) echo "Cette mise à jour comprend diverses améliorations et corrections de bugs pour améliorer l'application pour vous." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *gu*) echo "આ અપડેટમાં વિવિધ સુધારાઓ અને બગ ફિક્સનો સમાવેશ થાય છે જે એપ્લિકેશનને તમારા માટે વધુ સારું બનાવે છે." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *he*) echo "עדכון זה כולל שיפורים ותיקוני באגים שונים כדי להפוך את האפליקציה לטובה יותר עבורך." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *hi-IN*) echo "इस अपडेट में विभिन्न सुधार और बग फिक्स शामिल हैं जो ऐप को आपके लिए बेहतर बनाते हैं।" >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *hr*) echo "Ovo ažuriranje uključuje razna poboljšanja i ispravke pogrešaka kako bi aplikacija bila bolja za vas." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *hu-HU*) echo "Ez a frissítés különféle fejlesztéseket és hibajavításokat tartalmaz, hogy jobbá tegye az alkalmazást az Ön számára." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *is-IS*) echo "Þessi uppfærsla inniheldur ýmsar endurbætur og villuleiðréttingar til að gera forritið betra fyrir þig." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *it-IT*) echo "Questo aggiornamento include vari miglioramenti e correzioni di bug per rendere l'app migliore per te." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *ja-JP*) echo "このアップデートには、アプリをより良くするためのさまざまな改善とバグ修正が含まれています。" >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *ko*) echo "이번 업데이트에는 다양한 개선 사항과 버그 수정이 포함되어 있어 앱을 더 잘 사용할 수 있습니다." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *pl-PL*) echo "Ta aktualizacja zawiera różne ulepszenia i poprawki błędów, aby aplikacja była lepsza dla ciebie." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *ru*) echo "Это обновление включает в себя различные улучшения и исправления ошибок, чтобы сделать приложение лучше для вас." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *sq*) echo "Ky përditësim përfshin përmirësime dhe riparime të ndryshme për ta bërë aplikacionin më të mirë për ju." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *sr*) echo "Ovo ažuriranje uključuje razna poboljšanja i ispravke pogrešaka kako bi aplikacija bila bolja za vas." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *sv*) echo "Denna uppdatering inkluderar olika förbättringar och buggfixar för att göra appen bättre för dig." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *tr-TR*) echo "Bu güncelleme, uygulamayı sizin için daha iyi hale getirmek için çeşitli iyileştirmeler ve hata düzeltmeleri içerir." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *uk*) echo "Це оновлення включає різні покращення та виправлення помилок, щоб зробити додаток кращим для вас." >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *zh-CN*) echo "此更新包括各种改进和错误修复，以使本 App 更好用。" >> "$DIR/${BUILD_NUMBER}.txt" ;;
-    *zh-TW*) echo "此更新包括各種改進和錯誤修復，以使本 App 更好用。" >> "$DIR/${BUILD_NUMBER}.txt" ;;
+    *ar*) echo "يتضمن هذا التحديث تحسينات وإصلاحات للأخطاء لجعل التطبيق أفضل بالنسبة لك." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *bg*) echo "Това актуализиране включва различни подобрения и корекции на грешки, за да направи приложението по-добро за вас." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *bo*) echo "ད་དུང་འདི་ལག་ཐོག་སྤུན་རྒྱུད་བཟོ་བཅོས་དང་བདག་སྤྱོད་གཏོང་མཁན་སོགས་ནང་འཁོད་དང་འབྲི་གཏོང་མཁན་སྔར་བཞིན་གཞན་དང་མཉམ་འབྲི་འདུག་གས།" >> $DIR/${BUILD_NUMBER}.txt ;;
+    *cs-CZ*) echo "Tato aktualizace obsahuje různá vylepšení a opravy chyb, aby byla aplikace pro vás lepší." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *de-DE*) echo "Dieses Update umfasst verschiedene Verbesserungen und Fehlerbehebungen, um die App für dich zu verbessern." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *en-US*) echo "This update includes various improvements and bug fixes to make the app better for you." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *es-ES*) echo "Esta actualización incluye varias mejoras y correcciones de errores para mejorar la aplicación para ti." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *fa-IR*) echo "این به‌روزرسانی شامل بهبودها و رفع اشکالات مختلفی است که برنامه را برای شما بهتر می‌کند." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *fr*) echo "Cette mise à jour comprend diverses améliorations et corrections de bugs pour améliorer l'application pour vous." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *gu*) echo "આ અપડેટમાં વિવિધ સુધારાઓ અને બગ ફિક્સનો સમાવેશ થાય છે જે એપ્લિકેશનને તમારા માટે વધુ સારું બનાવે છે." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *he*) echo "עדכון זה כולל שיפורים ותיקוני באגים שונים כדי להפוך את האפליקציה לטובה יותר עבורך." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *hi-IN*) echo "इस अपडेट में विभिन्न सुधार और बग फिक्स शामिल हैं जो ऐप को आपके लिए बेहतर बनाते हैं।" >> $DIR/${BUILD_NUMBER}.txt ;;
+    *hr*) echo "Ovo ažuriranje uključuje razna poboljšanja i ispravke pogrešaka kako bi aplikacija bila bolja za vas." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *hu-HU*) echo "Ez a frissítés különféle fejlesztéseket és hibajavításokat tartalmaz, hogy jobbá tegye az alkalmazást az Ön számára." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *is-IS*) echo "Þessi uppfærsla inniheldur ýmsar endurbætur og villuleiðréttingar til að gera forritið betra fyrir þig." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *it-IT*) echo "Questo aggiornamento include vari miglioramenti e correzioni di bug per rendere l'app migliore per te." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *ja-JP*) echo "このアップデートには、アプリをより良くするためのさまざまな改善とバグ修正が含まれています。" >> $DIR/${BUILD_NUMBER}.txt ;;
+    *ko*) echo "이번 업데이트에는 다양한 개선 사항과 버그 수정이 포함되어 있어 앱을 더 잘 사용할 수 있습니다." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *pl-PL*) echo "Ta aktualizacja zawiera różne ulepszenia i poprawki błędów, aby aplikacja była lepsza dla ciebie." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *ru*) echo "Это обновление включает в себя различные улучшения и исправления ошибок, чтобы сделать приложение лучше для вас." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *sq*) echo "Ky përditësim përfshin përmirësime dhe riparime të ndryshme për ta bërë aplikacionin më të mirë për ju." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *sr*) echo "Ovo ažuriranje uključuje razna poboljšanja i ispravke pogrešaka kako bi aplikacija bila bolja za vas." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *sv*) echo "Denna uppdatering inkluderar olika förbättringar och buggfixar för att göra appen bättre för dig." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *tr-TR*) echo "Bu güncelleme, uygulamayı sizin için daha iyi hale getirmek için çeşitli iyileştirmeler ve hata düzeltmeleri içerir." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *uk*) echo "Це оновлення включає різні покращення та виправлення помилок, щоб зробити додаток кращим для вас." >> $DIR/${BUILD_NUMBER}.txt ;;
+    *zh-CN*) echo "此更新包括各种改进和错误修复，以使本 App 更好用。" >> $DIR/${BUILD_NUMBER}.txt ;;
+    *zh-TW*) echo "此更新包括各種改進和錯誤修復，以使本 App 更好用。" >> $DIR/${BUILD_NUMBER}.txt ;;
   esac
   #$EDITOR $DIR/${BUILD_NUMBER}.txt
 done
